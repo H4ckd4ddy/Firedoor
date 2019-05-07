df18dec80cff4c05ba5873746a244603e99cd132
Etienne Sellan <git@sellan.fr>
2019-05-07 19:13:48 +0200

    Move installation methods in a class

--- src/firedoor.py
+++ src/firedoor.py
@@ -18,6 +18,7 @@ import signal
 import json
 import shutil
 from shutil import copyfile
+from installer import *
 from config_database import database
 from firedoor_modules_manager import *
 from request_handler import request_handler
@@ -27,63 +28,6 @@ version = 4.0
 # GLOBAL INIT BEGIN
 config_directory = "/etc/firedoor/"
 # GLOBAL INIT END
-
-
-
-def install():
-	if not os.path.isdir(config_directory):
-		os.mkdir(config_directory)
-	if not os.path.exists(config_directory+'database.json'):
-		if hasattr(sys, '_MEIPASS'):
-			base_directory = getattr(sys, '_MEIPASS', os.getcwd())
-		else:
-			base_directory = os.path.dirname(os.path.realpath(__file__))
-		os.chdir(base_directory)
-		copyfile('default_database.json', config_directory+'database.json')
-	if not os.path.exists('/etc/systemd/system/firedoor.service') and os.path.isdir('/etc/systemd/system'):
-		with open('firedoor.service', 'r') as default_service_file:
-			service = default_service_file.read()
-			service = service.replace('{{firedoor_path}}', sys.executable)
-			with open('/etc/systemd/system/firedoor.service', 'w+') as service_file:
-				service_file.write(service)
-		os.system('systemctl enable firedoor.service')
-	initialisation()
-	modules_manager.install_modules()
-
-def initialisation():
-	database.init(config_directory+'database.json')
-	database.set('config_directory', config_directory)
-	if hasattr(sys, '_MEIPASS'):
-		database.set('base_directory', getattr(sys, '_MEIPASS', os.getcwd()))
-	else:
-		database.set('base_directory', os.path.dirname(os.path.realpath(__file__)))
-	os.chdir(database.get('base_directory'))
-	modules_manager.import_modules()
-
-def uninstall():
-	if confirm_uninstall():
-		modules_manager.uninstall_modules()
-		if os.path.isdir(config_directory):
-			shutil.rmtree(config_directory)
-		print('Firedoor successfully uninstalled !')
-
-def confirm_uninstall():
-	choice = ''
-	while choice not in ['y', 'n']:
-		choice = input('Uninstall Firedoor and delete all configurations [y/n] ? ').lower()
-	if choice == 'y':
-		return True
-	else:
-		print('/!\\ Uninstallation aborted /!\\')
-
-
-
-
-
-
-
-
-
 
 def run_on():
 	print('\n')
@@ -109,23 +53,24 @@ def start_server():
 	signal.pause()
 	
 def main():
-	if os.path.exists(config_directory+'database.json'):
-		initialisation()
-		if len(sys.argv) > 1:
-			if sys.argv[1] == 'uninstall':
-				uninstall()
-			else:
-				modules_manager.run_cli_module(sys.argv[1], sys.argv[2:])
+	if len(sys.argv) > 1:
+		if sys.argv[1] == 'install':
+			installer.install(config_directory)
+		elif sys.argv[1] == 'uninstall':
+			installer.uninstall(config_directory)
+		elif installer.check_installation(config_directory):
+			installer.initialisation(config_directory)
+			modules_manager.run_cli_module(sys.argv[1], sys.argv[2:])
 		else:
-			modules_manager.startup_modules()
-			start_server()
+			print('Firedoor is not correctly installed')
+			exit()
+	elif installer.check_installation(config_directory):
+		installer.initialisation(config_directory)
+		modules_manager.startup_modules()
+		start_server()
 	else:
-		if len(sys.argv) > 1:
-			if sys.argv[1] == 'install':
-				install()
-			else:
-				print('Firedoor is not correctly installed')
-				exit()
+		print('Firedoor is not correctly installed')
+		exit()
 
 if __name__ == '__main__':
 	main()
---
+++ src/installer.py
@@ -0,0 +1,70 @@
+import os
+import shutil
+from shutil import copyfile
+from firedoor_modules_manager import *
+
+class installer:
+	
+	@classmethod
+	def initialisation(cls, config_directory):
+		database.init(config_directory+'database.json')
+		database.set('config_directory', config_directory)
+		database.set('base_directory', cls.get_exe_path())
+		os.chdir(database.get('base_directory'))
+		modules_manager.import_modules()
+	
+	@classmethod
+	def install(cls, config_directory):
+		if not os.path.isdir(config_directory):
+			os.mkdir(config_directory)
+		if not os.path.exists(config_directory+'database.json'):
+			if hasattr(sys, '_MEIPASS'):
+				base_directory = getattr(sys, '_MEIPASS', os.getcwd())
+			else:
+				base_directory = os.path.dirname(os.path.realpath(__file__))
+			os.chdir(base_directory)
+			copyfile('default_database.json', config_directory+'database.json')
+		if not os.path.exists('/etc/systemd/system/firedoor.service') and os.path.isdir('/etc/systemd/system'):
+			with open('firedoor.service', 'r') as default_service_file:
+				service = default_service_file.read()
+				service = service.replace('{{firedoor_path}}', sys.executable)
+				with open('/etc/systemd/system/firedoor.service', 'w+') as service_file:
+					service_file.write(service)
+			os.system('systemctl enable firedoor.service')
+		cls.initialisation(config_directory)
+		modules_manager.install_modules()
+		print('Firedoor successfully installed !')
+	
+	@classmethod
+	def uninstall(cls, config_directory):
+		cls.initialisation(config_directory)
+		if cls.confirm_uninstall():
+			modules_manager.uninstall_modules()
+			if os.path.isdir(config_directory):
+				shutil.rmtree(config_directory)
+			print('Firedoor successfully removed !')
+	
+	@classmethod
+	def confirm_uninstall(cls):
+		choice = ''
+		while choice not in ['y', 'n']:
+			choice = input('Uninstall Firedoor and delete all configurations [y/n] ? ').lower()
+		if choice == 'y':
+			return True
+		else:
+			print('/!\\ Uninstallation aborted /!\\')
+	
+	@classmethod
+	def get_exe_path(cls):
+		if hasattr(sys, '_MEIPASS'):
+			base_directory = getattr(sys, '_MEIPASS', os.getcwd())
+		else:
+			base_directory = os.path.dirname(os.path.realpath(__file__))
+		return base_directory
+	
+	@classmethod
+	def check_installation(cls, config_directory='/etc/firedoor/'):
+		if os.path.exists(config_directory+'database.json'):
+			return True
+		else:
+			return False
