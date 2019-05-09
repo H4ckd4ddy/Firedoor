c0e01445435a6aece55eeb864081cc08e9344620
Etienne SELLAN <git@sellan.fr>
2019-05-09 21:49:54 +0200

    Remove useless methods in WAF

--- src/modules/WAF/WAF.py
+++ src/modules/WAF/WAF.py
@@ -24,8 +24,6 @@ class WAF():
 	
 	@staticmethod
 	def start(database):
-		#WAF.SQLI_pattern = re.compile(r"'\s*(AND|OR|XOR|&&|\|\|)\s*('|[0-9]|`?[a-z\._-]+`?\s*=|[a-z]+\s*\()")
-		#WAF.XSS_pattern = re.compile(r"(\b)(on\S+)(\s*)=|javascript|(<\s*)(\/*)script")
 		analyzers_manager.import_analyzers()
 		WAF.thread = Thread(target = WAF.analyser, args=[database])
 		WAF.thread.daemon = True
@@ -42,7 +40,6 @@ class WAF():
 	def analyser(database):
 		WAF.database = database
 		sniff(prn=analyzers_manager.packets_handler, store=0, count=0, stop_filter=WAF.check_state)
-		#sniff(filter='tcp', prn=WAF.packet_callback, store=0, count=0, stop_filter=WAF.check_state)
 	
 	@staticmethod
 	def check_state(packet):
@@ -50,55 +47,6 @@ class WAF():
 			return True
 		else:
 			return False
-	
-	"""
-	@staticmethod
-	def packet_callback(packet):
-		if TCP in packet:
-			if packet[TCP].payload:
-				if packet[IP].dport == 8080:
-					WAF.check_packet(packet)
-	
-	@staticmethod
-	def check_packet(packet):
-		WAF.check_XSS(packet)
-		WAF.check_SQLI(packet)
-	
-	
-	@staticmethod
-	def import_analyzers():
-		if os.path.isdir('analyzers'):
-			for analyzer_name in os.listdir('analyzers'):
-				if analyzer_name not in WAF.analyzers:
-					WAF.analyzers[analyzer_name] = module(directory, module_name)
-	
-	
-	@staticmethod
-	def check_XSS(packet):
-		try:
-			payload = bytes(packet[TCP].payload)
-			if sum(payload) == 0:
-				return
-			load = str(payload)
-			data = urllib.parse.unquote(urllib.parse.unquote(urllib.parse.unquote(urllib.parse.unquote(load))))
-			if WAF.XSS_pattern.search(data):
-				WAF.database.runtime_space['report_ip'](packet[IP].src, 3, 'Cross-site scripting')
-		except:
-			pass
-	
-	@staticmethod
-	def check_SQLI(packet):
-		try:
-			payload = bytes(packet[TCP].payload)
-			if sum(payload) == 0:
-				return
-			load = str(payload)
-			data = urllib.parse.unquote(urllib.parse.unquote(urllib.parse.unquote(urllib.parse.unquote(load))))
-			if WAF.SQLI_pattern.search(data):
-				WAF.database.runtime_space['report_ip'](packet[IP].src, 5, 'SQL injection')
-		except:
-			pass
-	"""
 	
 	@staticmethod
 	def web_entrypoint(database, client_ip, get, post):
