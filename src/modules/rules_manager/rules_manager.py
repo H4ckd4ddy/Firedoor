a2df7b76560d1fe5a66a35f386b81e3e52c8e4a4
Etienne SELLAN <git@sellan.fr>
2019-05-10 19:42:05 +0200

    Add rules change saving

--- src/modules/rules_manager/rules_manager.py
+++ src/modules/rules_manager/rules_manager.py
@@ -1,17 +1,20 @@
 from urllib.parse import unquote
+import json
 
 class rules_manager():
 	
 	@staticmethod
 	def web_entrypoint(database, client_ip, get, post):
-		if len(get) > 0:
+		if 'rules' in post:
+			with open(database.get('config_directory')+'static_rules.conf', 'w') as rules_file:
+				new_rules = unquote(unquote(post['rules'])).replace('+', ' ')
+				new_rules = json.dumps(json.loads(new_rules), indent=4)
+				rules_file.write(new_rules)
+				return 200,'OK'
+		elif len(get) > 0:
 			if get[0] == 'rules':
 				with open(database.get('config_directory')+'static_rules.conf', 'r') as rules_file:
 					return 200, rules_file.read()
-		if 'rules' in post:
-			with open(database.get('config_directory')+'static_rules.conf', 'w') as rules_file:
-				rules_file.write(unquote(unquote(post['rules'])).replace('+', ' '))
-				return 200,'OK'
 		return rules_manager.return_interface(database)
 	
 	@staticmethod
