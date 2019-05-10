4b4492cda1074936bb7d9fe3a32f3aefe6db661f
Etienne SELLAN <git@sellan.fr>
2019-05-10 18:44:07 +0200

    Change static rules display

--- src/modules/rules_manager/rules_manager.py
+++ src/modules/rules_manager/rules_manager.py
@@ -4,9 +4,14 @@ class rules_manager():
 	
 	@staticmethod
 	def web_entrypoint(database, client_ip, get, post):
+		if len(get) > 0:
+			if get[0] == 'rules':
+				with open(database.get('config_directory')+'static_rules.conf', 'r') as rules_file:
+					return 200, rules_file.read()
 		if 'rules' in post:
 			with open(database.get('config_directory')+'static_rules.conf', 'w') as rules_file:
 				rules_file.write(unquote(unquote(post['rules'])).replace('+', ' '))
+				return 200,'OK'
 		return rules_manager.return_interface(database)
 	
 	@staticmethod
@@ -14,5 +19,4 @@ class rules_manager():
 		with open('interface.html', 'r') as interface:
 			with open(database.get('config_directory')+'static_rules.conf') as rules_file:  
 				html = interface.read()
-				html = html.replace('{{static_rules}}', rules_file.read())
 				return 200, html
